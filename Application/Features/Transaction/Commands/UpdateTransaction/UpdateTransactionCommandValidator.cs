f71536fedf9d5e1f1f26b0904abb1b6321d44d81
Osama Alsadi <diacubic@hotmail.com>
2022-07-04 14:37:37 +0300

    ...

--- Application/Features/Transaction/Commands/UpdateTransaction/UpdateTransactionCommandValidator.cs
+++ Application/Features/Transaction/Commands/UpdateTransaction/UpdateTransactionCommandValidator.cs
@@ -1,13 +1,17 @@
 ﻿using Application.Common.Interfaces;
+using Application.Features.Category.Commands.CreateCategory;
 using FluentValidation;
 
 namespace Application.Features.Transaction.Commands.UpdateTransaction;
 
 public class UpdateTransactionCommandValidator : AbstractValidator<UpdateTransactionCommand>
 {
-    public UpdateTransactionCommandValidator()
+    public UpdateTransactionCommandValidator(IValidator<CreateCategoryCommand> createCategoryCommandValidator)
     {
         RuleFor(v => v.Description)
             .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+        RuleFor(v => v.CreateCategoryCommand!)
+            .SetValidator(createCategoryCommandValidator);
     }
 }
